71b50238fee15eb7345870dbdb38806b21a54004
simka <esemukov09@gmail.com>
2026-05-29 05:21:39 +0300

    Первый коммит: инициализация проекта

---
+++ MyWebPageApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace MyWebPageApp.Tests;
+
+public class SampleTests
+{
+    [Fact]
+    public void TrueShouldBeTrue_Test()
+    {
+        // Простейший тест для проверки CI-конвейера
+        Assert.Equal(1, 1);
+    }
+}
